31bf47affbe11492f090d5304d7bf2064a578239
dmitry.bilyk <dmitry.bilyk@gmail.com>
2014-04-29 17:03:26 +0300

    jquery

--- src/main/java/com/learn/ui/jquery/learn27042014/customEvent.js
+++ src/main/java/com/learn/ui/jquery/learn27042014/customEvent.js
@@ -5,4 +5,12 @@ $(function(){
     $("input").on('click',function(){
         $(this).trigger('customEvent');
     });
+
+    $('input#submitButton').on('myEvent', function(event){
+        event.preventDefault;
+        alert("my event is working");
+    })
+    $('input#submitButton').on('click',  function(){
+        $(this).trigger('myEvent')
+    })
 });
