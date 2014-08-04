ab2baaceb91af969c11523670578971b4227012c
dmitry.bilyk <dmitry.bilyk@symmetrics.de>
2014-08-04 18:21:11 +0300

    adapter

---
+++ src/main/java/com/learn/patterns/freemanAndFreeman/headfirst/adapter/custom/Main.java
@@ -0,0 +1,12 @@
+package com.learn.patterns.freemanAndFreeman.headfirst.adapter.custom;
+
+/**
+ * Created by bid on 8/4/14.
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+
+   }
+}
