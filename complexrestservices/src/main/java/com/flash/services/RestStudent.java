3237f309eec949ed3a289adfd6cfe48b31a2c198
dmitry.bilyk <dmitry.bilyk@symmetrics.de>
2014-08-05 18:54:12 +0300

    rest

--- complexrestservices/src/main/java/com/flash/services/RestStudent.java
+++ complexrestservices/src/main/java/com/flash/services/RestStudent.java
@@ -1,15 +1,21 @@
 package com.flash.services;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 /**
  * Created by bid on 8/5/14.
  */
-@Path("/student")
+@Path("/")
 public interface RestStudent
 {
    public void printStudent();
 
+   @Path("create")
+   @Produces("application/json;charset=UTF-8")
    public Response createStudent();
 }
